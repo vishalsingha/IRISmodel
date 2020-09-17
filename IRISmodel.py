abbc20e2c9b0d78d20d1615b0e54d4a4f9ed844d
VISHAL SINGH <56965195+vishalsingha@users.noreply.github.com>
2020-09-17 13:22:34 +0530

    Add files via upload

---
+++ IRISmodel.py
@@ -0,0 +1,40 @@
+# import library
+import numpy as np 
+import pandas as pd 
+import pickle 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# read data
+df = pd.read_csv("C:\\Users\\Vishal Singh\\Desktop\\IRIS.csv")
+
+# get label
+Y = df['species'].astype('category').cat.codes
+
+# preprocess data
+s = StandardScaler()
+
+X = df.drop(['species'] ,axis = 1)
+s.fit(X)
+pickle.dump(s, open('standardscaler.pkl', 'wb'))
+X = s.transform(X)
+
+# split train and test data
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2)
+
+
+# creating classifier
+clf = LogisticRegression()
+clf.fit(x_train, y_train)
+
+# print model accuracy
+print(clf.score(x_test, y_test))
+print(x_test)
+print("#"*20)
+print(clf.predict(x_test))
+
+
+
+# strlise model
+pickle.dump(clf, open('model.pkl', 'wb'))
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app=Flask(__name__,template_folder='templates')
+
+model = pickle.load(open('C:\\Users\\Vishal Singh\\Documents\\work\\mypyhton\\Projects\\IRIS\\model.pkl', 'rb'))
+s = pickle.load(open('C:\\Users\\Vishal Singh\\Documents\\work\\mypyhton\\Projects\\IRIS\\standardscaler.pkl', 'rb'))
+map_dict = {
+    0:"Iris-setosa",
+    1:'Iris-versicolor',
+    2:'Iris-virginica'
+}
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    arr = [float(x) for x in request.form.values()]
+    arr = [np.array(arr)]
+    arr = s.transform(arr)
+    pred = model.predict(arr)
+    return render_template('index.html', prediction_text = f'This is a {map_dict[pred[0]]} type of flower')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
